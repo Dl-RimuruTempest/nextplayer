8dfc4bd75f9f8425e62a04e02e3e7676484bf2ed
Dl-RimuruTempest <129542054+Dl-RimuruTempest@users.noreply.github.com>
2026-03-16 15:40:02 +0530

    Update ControlsTopView.kt

--- feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/ui/controls/ControlsTopView.kt
+++ feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/ui/controls/ControlsTopView.kt
@@ -1,13 +1,11 @@
 package dev.anilbeesetti.nextplayer.feature.player.ui.controls
 
 import androidx.annotation.OptIn
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.displayCutout
-import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.union
@@ -17,25 +15,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.media3.common.util.UnstableApi
+import dev.anilbeesetti.nextplayer.core.model.VideoContentScale
 import dev.anilbeesetti.nextplayer.core.ui.R
 import dev.anilbeesetti.nextplayer.core.ui.extensions.copy
 import dev.anilbeesetti.nextplayer.feature.player.buttons.PlayerButton
+import dev.anilbeesetti.nextplayer.feature.player.extensions.drawableRes
 
 @OptIn(UnstableApi::class)
 @Composable
 fun ControlsTopView(
     modifier: Modifier = Modifier,
     title: String,
-    onAudioClick: () -> Unit = {},
-    onSubtitleClick: () -> Unit = {},
+    videoContentScale: VideoContentScale,
     onPlaybackSpeedClick: () -> Unit = {},
     onPlaylistClick: () -> Unit = {},
+    onPictureInPictureClick: () -> Unit = {},
+    onVideoContentScaleClick: () -> Unit = {},
+    onVideoContentScaleLongClick: () -> Unit = {},
     onBackClick: () -> Unit,
 ) {
     val systemBarsPadding = WindowInsets.systemBars.union(WindowInsets.displayCutout).asPaddingValues()
@@ -61,32 +62,38 @@ fun ControlsTopView(
             overflow = TextOverflow.Ellipsis,
             modifier = Modifier.weight(1f),
         )
-
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-            PlayerButton(onClick = onPlaylistClick) {
-                Icon(
-                    painter = painterResource(R.drawable.ic_playlist),
-                    contentDescription = null,
-                )
-            }
+            // Playback Speed
             PlayerButton(onClick = onPlaybackSpeedClick) {
                 Icon(
                     painter = painterResource(R.drawable.ic_speed),
                     contentDescription = null,
                 )
             }
-            PlayerButton(onClick = onAudioClick) {
+            // Playlist
+            PlayerButton(onClick = onPlaylistClick) {
                 Icon(
-                    painter = painterResource(R.drawable.ic_audio_track),
+                    painter = painterResource(R.drawable.ic_playlist),
                     contentDescription = null,
                 )
             }
-            PlayerButton(onClick = onSubtitleClick) {
+            // Picture-in-Picture
+            PlayerButton(onClick = onPictureInPictureClick) {
                 Icon(
-                    painter = painterResource(R.drawable.ic_subtitle_track),
+                    painter = painterResource(R.drawable.ic_pip),
+                    contentDescription = null,
+                )
+            }
+            // Video Zoom / Content Scale
+            PlayerButton(
+                onClick = onVideoContentScaleClick,
+                onLongClick = onVideoContentScaleLongClick,
+            ) {
+                Icon(
+                    painter = painterResource(videoContentScale.drawableRes()),
                     contentDescription = null,
                 )
             }
